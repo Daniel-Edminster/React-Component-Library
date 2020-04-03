9e0b0add6e7c955bd0567b01c20cfcc23445b349
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-03 13:29:40 -0400

    completed buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,36 @@ import React from 'react';
 import './Button.css';
 
 const Button = (props) => {
- 
+    let classList = "";
+
+    let types = [
+        'primary','primary-hollow','primary-muted',
+        'success','success-hollow','success-muted',
+        'warning','warning-hollow','warning-muted',
+        'danger','danger-hollow','danger-muted',
+        'default','default-hollow','default-muted',
+        'cart-text', 'cart', 'favorites-text', 'favorites'
+    ];
+
+    if(props.type)
+    {
+        if(types.includes(props.type))
+        {
+            classList += ` button-${props.type}`;
+        }
+        else {
+            classList += ` button-default`;
+        }
+
+    }
+
+    if(props.size)
+    {
+        if(props.size === "large")
+        {
+            classList += " button-lg";
+        }
+    }
  
  
  
