0a9c2603f1104de2462f275c9578afed8305bcbc
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-03 08:17:19 -0400

    Got started on buttons

---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './fonts.css';
+import './Button.css';
+
+const Button = (props) => {
+ 
+ 
+ 
+ 
+ 
+    return (
+    <button className={classList}>
+        {props.label}
+    </button> )
+}
+
+export default Button; 
