afdf1944aecfdf000ee2e3ec7d313233eb278fbd
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-26 19:40:50 -0400

    built static storybook

---
+++ .out/precache-manifest.36145e3cdd297757fa9843f8b9c142b0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cdbe75797c68bab591da3430a03686",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.54638381ae51e1eadfcf.bundle.js"
+  },
+  {
+    "url": "runtime~main.54638381ae51e1eadfcf.bundle.js"
+  },
+  {
+    "revision": "67bb13f8fb8bdbcfb5c9",
+    "url": "static/css/main.3b2f4c71.chunk.css"
+  },
+  {
+    "revision": "ae62043af9e6fe101a2aeab9a0a346f3",
+    "url": "static/media/CartCircle.ae62043a.svg"
+  },
+  {
+    "revision": "51f545402ed8807cc8b39933c4e02377",
+    "url": "static/media/CartCircleHollow.51f54540.svg"
+  },
+  {
+    "revision": "ac5f4558485f389e9318025eeaed7aa0",
+    "url": "static/media/HeartCircle.ac5f4558.svg"
+  },
+  {
+    "revision": "36d3923bb73b7f1e2143241f2b63948c",
+    "url": "static/media/HeartCircleHollow.36d3923b.svg"
+  },
+  {
+    "revision": "7695d2b4e548776b42c1676d408a70fa",
+    "url": "static/media/cart_2.7695d2b4.png"
+  },
+  {
+    "revision": "c7538c30c8d8117b872fa29c537f303f",
+    "url": "static/media/headphones.c7538c30.png"
+  },
+  {
+    "revision": "7a05ce7dd38c7e9267e3ccbf78f468cd",
+    "url": "static/media/hearts.7a05ce7d.png"
+  },
+  {
+    "revision": "38253f2b7f66997eaf43ca97cb58e08a",
+    "url": "static/media/laptop.38253f2b.svg"
+  },
+  {
+    "revision": "b8934614ef79756e599fb2d2e59487a0",
+    "url": "static/media/starGrey.b8934614.svg"
+  },
+  {
+    "revision": "dee1edc93e04317551d3d1b7f3ecb069",
+    "url": "static/media/starYellow.dee1edc9.svg"
+  },
+  {
+    "revision": "f332df5201d66014d1d742490d6d8d0b",
+    "url": "static/media/v.f332df52.svg"
+  },
+  {
+    "url": "vendors~main.54638381ae51e1eadfcf.bundle.js"
+  },
+  {
+    "url": "vendors~main.54638381ae51e1eadfcf.bundle.js.LICENSE.txt"
+  }
+]);
---
+++ storybook-static/precache-manifest.41631874014aacdfcf938074202f361e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8014fe0c31e62f026972881aa68b1458",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7f6ba539f674a4593238.bundle.js"
+  },
+  {
+    "url": "runtime~main.7f6ba539f674a4593238.bundle.js"
+  },
+  {
+    "revision": "4a97b30d3bdfcfe761c8",
+    "url": "static/css/main.62705c36.chunk.css"
+  },
+  {
+    "revision": "ae62043af9e6fe101a2aeab9a0a346f3",
+    "url": "static/media/CartCircle.ae62043a.svg"
+  },
+  {
+    "revision": "51f545402ed8807cc8b39933c4e02377",
+    "url": "static/media/CartCircleHollow.51f54540.svg"
+  },
+  {
+    "revision": "ac5f4558485f389e9318025eeaed7aa0",
+    "url": "static/media/HeartCircle.ac5f4558.svg"
+  },
+  {
+    "revision": "36d3923bb73b7f1e2143241f2b63948c",
+    "url": "static/media/HeartCircleHollow.36d3923b.svg"
+  },
+  {
+    "revision": "7695d2b4e548776b42c1676d408a70fa",
+    "url": "static/media/cart_2.7695d2b4.png"
+  },
+  {
+    "revision": "c7538c30c8d8117b872fa29c537f303f",
+    "url": "static/media/headphones.c7538c30.png"
+  },
+  {
+    "revision": "7a05ce7dd38c7e9267e3ccbf78f468cd",
+    "url": "static/media/hearts.7a05ce7d.png"
+  },
+  {
+    "revision": "38253f2b7f66997eaf43ca97cb58e08a",
+    "url": "static/media/laptop.38253f2b.svg"
+  },
+  {
+    "revision": "b8934614ef79756e599fb2d2e59487a0",
+    "url": "static/media/starGrey.b8934614.svg"
+  },
+  {
+    "revision": "dee1edc93e04317551d3d1b7f3ecb069",
+    "url": "static/media/starYellow.dee1edc9.svg"
+  },
+  {
+    "revision": "f332df5201d66014d1d742490d6d8d0b",
+    "url": "static/media/v.f332df52.svg"
+  },
+  {
+    "url": "vendors~main.7f6ba539f674a4593238.bundle.js"
+  },
+  {
+    "url": "vendors~main.7f6ba539f674a4593238.bundle.js.LICENSE.txt"
+  }
+]);
